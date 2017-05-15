475180b53ecd2f67cdc534668e78dc16f262cc07
t-moxa <t-moxa@mail.ru>
2017-05-15 10:41:02 +0500

    Add GetTime function

--- watchdog_proc.py
+++ watchdog_proc.py
@@ -5,19 +5,30 @@ def CheckProcStatus(proc_name='explorer.exe'):
         if process.name() == proc_name:
             #print("Proccess {} is running!".format(proc_name))
             return True
-CheckProcStatus()
+
+def GetTime():
+    time_list=list(time.localtime())[0:6]
+    time_string=""
+    time_string=str(time_list[0])+"/"+str(time_list[1])+"/"+str(time_list[2])+"; "+ \
+                 str(time_list[3])+":"+str(time_list[4])+":"+str(time_list[5])
+    return time_string
+
+##CheckProcStatus()
 flag=False
 flag2=False
 while True:
     time.sleep(1)
+##    print(GetTime())
     if CheckProcStatus('notepad.exe') and flag==False:
-        print("Notepad has been started ")
-        flag=True 
+        print("Notepad has been started at " + GetTime())
+        flag=True
+        
     elif flag==True and not CheckProcStatus('notepad.exe'):
         flag=False
         flag2=False
-        print("Notepad has been stopped")
+        print("Notepad has been stopped at " + GetTime())
     elif flag==False and flag2==False:
         print("Waiting")
         flag2=True
     
+print(GetTime())
