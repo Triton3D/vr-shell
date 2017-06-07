d880e14e1002f38bb9375cf2eef1707171649420
t-moxa <t-moxa@mail.ru>
2017-06-07 16:46:50 +0500

    Added remote DB write

--- test.py
+++ test.py
@@ -1,59 +1,9 @@
-##import cv2
-##DEV = 0
-##
-##if __name__ =='__main__':
-##    cv2.namedWindow('Camera',cv2.WINDOW_AUTOSIZE)
-##    capture=cv2.VideoCapture(0)
-##    key=''
-##    while key!='q':
-##        
-##        gray=cv2.cvtColor(1,cv2.COLOR_BAYER_BG2BGR)
-##        cv2.imshow('frame',gray)
-##        
-##        key=cv2.waitKey()
-##    capture.release()
-##    cv2.destroyAllWindows()    
-####    highgui.cvNamedWindow('Camera',highgui.CV_WINDOW_AUTOSIZE)
-####    highgui.cvMoveWindow('Camera',10,10)
-####    capture=highgui.cvCreateCameraCapture(DEV)
-####    key=''
-####    while key !='q':
-####        frame=highgui.cvQueryFrame(capture)
-####        highgui.cvShowImage('Camera',frame)
-####        key=highguicv.WaitKey(5)
-####
-##
-
-import cv2
-
-# Camera 0 is the integrated web cam on my netbook
-camera_port = 0
-
-#Number of frames to throw away while the camera adjusts to light levels
-ramp_frames = 30
-
-# Now we can initialize the camera capture object with the cv2.VideoCapture class.
-# All it needs is the index to a camera port.
-camera = cv2.VideoCapture(camera_port)
-
-# Captures a single image from the camera and returns it in PIL format
-def get_image():
-# read is the easiest way to get a full image out of a VideoCapture object.
-    retval, im = camera.read()
-    return im
-
-    # Ramp the camera - these frames will be discarded and are only used to allow v4l2
-    # to adjust light levels, if necessary
-    for i in xrange(ramp_frames):
-        temp = get_image()
-        print("Taking image...")
-        # Take the actual image we want to keep
-        camera_capture = get_image()
-        file = "c:/1.png"
-        # A nice feature of the imwrite method is that it will automatically choose the
-        # correct format based on the file extension you provide. Convenient!
-        cv2.imwrite(file, camera_capture)
-    
-    # You'll want to release the camera, otherwise you won't be able to create a new
-    # capture object until your script exits
-    del(camera)
+import subprocess
+def CheckInternetConnection():
+    cmd = 'ping ya.ru'
+    import subprocess
+    PIPE = subprocess.PIPE
+    p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE,
+            stderr=subprocess.STDOUT)
+    buf = p.stdout.read()
+    return 'TTL' in str(buf)
--- watchdog_proc.py
+++ watchdog_proc.py
@@ -3,10 +3,25 @@ from datetime import datetime,date,time
 import time
 import psycopg2
 
+DB_NAME='postgres'
+USER_NAM='postgres'
+PASSWORD='QwertyS123!'
+HOST_ADR='92.43.187.233'
+PORT_NUMB='5432'
+
 def CheckProcStatus(proc_name='explorer.exe'):
     for process in psutil.process_iter():
         if process.name() == proc_name:
             return True
+
+def CheckInternetConnection():
+    cmd = 'ping ya.ru'
+    import subprocess
+    PIPE = subprocess.PIPE
+    p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE,
+            stderr=subprocess.STDOUT)
+    buf = p.stdout.read()
+    return 'TTL' in str(buf)
 #def WriteToDB(db)
 
 proc_start=False
@@ -38,14 +53,14 @@ while True:
         start_time=str(start_time)[:-7]
         stop_time=str(stop_time)[:-7]
         duration=str(duration)[:-7]
-        con = psycopg2.connect(database='vr-shell',user='vr-man',password='QwertyS123!',host='localhost',port='5169')   #Write to DB
+        con = psycopg2.connect(database=DB_NAME,user=USER_NAM,password=PASSWORD,host=HOST_ADR,port=PORT_NUMB)   #Connect to DB
         with con:
             cur=con.cursor()
             cur.execute("CREATE TABLE IF NOT EXISTS Processes(Name text,StartTime timestamp without time zone,StopTime timestamp without time zone,Duration time without time zone)")
             cur.execute("INSERT INTO Processes VALUES('"+proc_name+"','"+start_time+"','"+stop_time+"','"+duration+"')")
         con.close()
     elif not proc_start and not proc_none:                   #Waiting for the process
-        print("Waiting")
+        print("Waiting for the process!")
         proc_none=True
    # print("End: "+str(datetime.now()))
 
